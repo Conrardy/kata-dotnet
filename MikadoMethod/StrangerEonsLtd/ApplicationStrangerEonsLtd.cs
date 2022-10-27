fdf3e887ef1ac6eb4ac02a3067f9b4186ad0c6d5
Emmanuel Conrardy <e.conrardy@oceasoft.lan>
2022-10-27 19:54:26 +0200

    DONE

---
+++ MikadoMethod/StrangerEonsLtd/ApplicationStrangerEonsLtd.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using System;
+
+namespace StrangerEonsLtd
+{
+    public class ApplicationStrangerEonsLtd : IApplication
+    {
+        public void Leet(string message, UI ui)
+        {
+            ui.SetLeeted(message.Replace("S","5"));
+        }
+        static void Main(string[] args)
+        {
+            UI ui = new UI(new ApplicationStrangerEonsLtd());
+            Console.WriteLine(ui.EncryptMessage(args[0]));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MikadoMethod/StrangerEonsLtd/StrangerEonsLtdTest.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrangerEonsLtd
+{
+    [TestClass]
+    public class StrangerEonsLtdTest
+    {
+        [TestMethod]
+        public void TestSecret()
+        {
+            UI ui = new UI(new ApplicationStrangerEonsLtd());
+            Assert.AreEqual("Leeted: 5ecret", ui.EncryptMessage("Secret"));
+        }
+    }
+}
